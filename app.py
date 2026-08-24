223d79df0db19a8083d60125940e20cdf2a99a4e
Sachin Kumar <sachinkumar@Sachins-MacBook-Air.local>
2026-08-24 18:29:54 +0530

    Allow network access on port 5001

--- app.py
+++ app.py
@@ -1010,9 +1010,8 @@ def file_too_large(error):
 # ============================================================
 
 if __name__ == "__main__":
-
     app.run(
-        host="127.0.0.1",
-        port=5000,
-        debug=True
+        host="0.0.0.0",
+        port=5001,
+        debug=False
     )
